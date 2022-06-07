a3bb64ba09736fc65627e783e1787f3bb20595c3
w00jimin <woojimin4131@naver.com>
2022-06-07 23:41:19 +0900

    initial commit

--- script/script.js
+++ script/script.js
@@ -12,13 +12,13 @@ $(document).ready(function(){
     var imgs = 2
     var now = 0;
     function start(){
-        $(".mainimg img").eq(0).siblings().fadeIn();
+        $(".mainimg").eq(0).siblings().fadeIn();
         setInterval(function(){slide();},3000);
     }
     function slide(){
         now = now==imgs?0:now+= 1;
         
-        $(".mainimg img").eq(now).fadeIn();
-        $(".mainimg img").eq(now-1).fadeOut();
+        $(".mainimg").eq(now).fadeIn();
+        $(".mainimg").eq(now-1).fadeOut();
     }
 });
