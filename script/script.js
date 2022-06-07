26b6c6b2920bb14e897db8c6cd640126fb6e558c
w00jimin <woojimin4131@naver.com>
2022-06-07 23:37:45 +0900

    initial commit

--- script/script.js
+++ script/script.js
@@ -6,3 +6,19 @@ $(document).ready(function(){
                 $(".sub").stop().slideUp();
     });
 });
+
+$(document).ready(function(){
+    start();
+    var imgs = 2
+    var now = 0;
+    function start(){
+        $(".mainimg img").eq(0).siblings().fadeIn();
+        setInterval(function(){slide();},3000);
+    }
+    function slide(){
+        now = now==imgs?0:now+= 1;
+        
+        $(".mainimg img").eq(now).fadeIn();
+        $(".mainimg img").eq(now-1).fadeOut();
+    }
+});
